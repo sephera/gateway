f3c0a4f0457260733b3b127879e60c36efe3ff62
haind6 <haind6@vng.com.vn>
2019-10-24 11:10:35 +0700

    add example gateway filter

--- src/main/java/com/example/gateway/gatewayfilter/PostProxyGatewayFilterFactory.java
+++ src/main/java/com/example/gateway/gatewayfilter/PostProxyGatewayFilterFactory.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 /**
@@ -19,13 +18,11 @@ public class PostProxyGatewayFilterFactory extends AbstractGatewayFilterFactory<
     @Override
     public GatewayFilter apply(PostProxyGatewayFilterFactory.Config config) {
         // grab configuration from Config object
-        return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                ServerHttpResponse response = exchange.getResponse();
-                //Manipulate the response in some way
-                response.getHeaders().add("X-Post-Proxy", "Gateway");
-            }));
-        };
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+            //Manipulate the response in some way
+            response.getHeaders().add("X-Post-Proxy", "Gateway");
+        }));
     }
 
     public static final class Config {
